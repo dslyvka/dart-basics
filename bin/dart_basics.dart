6944c318118c57e352f14b964f72c1f188d6e2a8
dslyvka <daniilslyvka@gmail.com>
2023-03-17 23:40:24 +0200

    lists

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,55 +1,53 @@
 void main(List<String> arguments) {
-  // class num => Numbers
-  int number = 5;
-  double numberDouble = 1.1;
-  double numberDouble1 = 1; // => 1.0
-  num numInstance = 1.0; // => 1.0
-  numInstance = 1; // => 1
+  // Lists
+  List fruits = ['apple', 'orange', 'kivi'];
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  var dNums = <double>[1.0, 2.0, 3.5];
 
-  // String => Strings
-  String string = "I am string";
-  String singleQuotes = "I am single quotes string";
-  String diffQuotesString = "I am diff 'quotes' string";
-  String diffQuotesString1 = 'I am diff \'quotes\' string';
-  String multylineString = "I am \n multiline string";
-  String multylineString1 = """I am
-multiline string""";
-  String interpolatedString = "$string which was interpolated ${1 + 1}";
+  fruits.add('lemon'); // => adding el to the end
+  fruits.insert(0, "banana"); // => adding el by index fruits[0] is banana
+  fruits.remove("apple"); // => removing first finded el
+  fruits.removeAt(0); // => removing el by index
 
-  // bool => Booleans
-  bool boolean = true;
-  boolean = false;
+  // Mutable lists
+  // var, int, ...
 
-  // var
-  var variable = "I am variable";
-  // variable = 1; => is not assignable to type int
+  // Partially mutable lists
+  final List<String> fruitsArray = ["apple", "banana", "lemon"];
+  fruitsArray.add("pineapple");
+  fruits[0] = "strawberry";
 
-  // dynamic
-  dynamic dynamicVariable = "I am dynamic variable";
-  dynamicVariable = 1; // => is assinable to all types
+  // Immutable lists
+  const List<String> rgb = ["255", "160", "250"];
+  // rgb.add("70"); => Unsupported operation: Cannot add to an unmodifiable list
+  // etc...
 
-  var dynamicVarVariable; // => dyncamic
-  dynamicVarVariable = 1;
-  dynamicVarVariable = "I am dynamic";
+  // final-const lists
+  // final List<DateTime> finalConstList = const[DateTime.now(), DateTime.now()]; // => error
+  final List<DateTime> finalList = [DateTime.now(), DateTime.now()];
+  final List<DateTime> unmodifiableList = List.unmodifiable(finalList);
 
-  // Runes UTF-32
-  Runes rune = Runes("Hello, Dart!");
-  // print(rune); => (72, 101, 108, 108, 111, 44, 32, 68, 97, 114, 116, 33)
-  // unicode array in UTF-32
+  // list properties
+  List<int> list = [0, 1, 2, 3, 4, 5];
+  list.first; // => arr[0]
+  list.last; // => arr[arr.length - 1]
+  list.isEmpty; // => false
+  list.isNotEmpty; // => true
 
-  // Symbol
-  Symbol varSymbol = #Iamsymbol;
+  //ForIn loop
+  // for (var num in list) {
+  //   num = 0;
+  // }
+  // print(list);
+  // Not working; We can not assign a value in for in loop
 
-  // Null
-  Null nullVar = null;
-  int? nullableVar = 6; // or null
-  nullableVar = null;
+  // if and for inside list
+  const List<int> numbersList = [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5];
+  final List<int> squaredNumbersList = [
+    for (var num in numbersList)
+      if (!num.isNegative) num * num
+  ];
+  // print(squaredNumbersList); => [1, 4, 9, 16, 25]
 
-  // Casting
-  num numVar = 110;
-  // numVar.isEven => error
-  // (numVar as int).isEven;
-
-  double doubleVar = numVar.toDouble();
-  String stringVar = numVar.toString();
+  
 }
