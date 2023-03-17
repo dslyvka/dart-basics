535a9d01b03dfafb06f06d179d6d2e3d65668db0
dslyvka <daniilslyvka@gmail.com>
2023-03-18 00:11:37 +0200

    sets

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,53 +1,21 @@
 void main(List<String> arguments) {
-  // Lists
-  List fruits = ['apple', 'orange', 'kivi'];
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  var dNums = <double>[1.0, 2.0, 3.5];
+  // Sets
+  Set<int> set = {1, 2, 3, 4};
+  var set1 = <int>{1, 2, 3, 4};
+  var set2 = {1, 2, 3, 3}; // =>  {1, 2, 3}; Set ignore duplicates
 
-  fruits.add('lemon'); // => adding el to the end
-  fruits.insert(0, "banana"); // => adding el by index fruits[0] is banana
-  fruits.remove("apple"); // => removing first finded el
-  fruits.removeAt(0); // => removing el by index
+  Set<int> set3 = Set.from([1, 2, 3, 3, 3, 4, 4, 5]);
+  // print(set3); => {1, 2, 3, 4, 5}
 
-  // Mutable lists
-  // var, int, ...
+  set3.contains(1); // => true
+  set3.add(7);
+  set3.remove(7);
+  set3.addAll([100, 45, 87]);
 
-  // Partially mutable lists
-  final List<String> fruitsArray = ["apple", "banana", "lemon"];
-  fruitsArray.add("pineapple");
-  fruits[0] = "strawberry";
+  Set<int> set4 = {...set3}; // copy
+  Set<int> set5 = set3.toSet(); // copy
 
-  // Immutable lists
-  const List<String> rgb = ["255", "160", "250"];
-  // rgb.add("70"); => Unsupported operation: Cannot add to an unmodifiable list
-  // etc...
-
-  // final-const lists
-  // final List<DateTime> finalConstList = const[DateTime.now(), DateTime.now()]; // => error
-  final List<DateTime> finalList = [DateTime.now(), DateTime.now()];
-  final List<DateTime> unmodifiableList = List.unmodifiable(finalList);
-
-  // list properties
-  List<int> list = [0, 1, 2, 3, 4, 5];
-  list.first; // => arr[0]
-  list.last; // => arr[arr.length - 1]
-  list.isEmpty; // => false
-  list.isNotEmpty; // => true
-
-  //ForIn loop
-  // for (var num in list) {
-  //   num = 0;
-  // }
-  // print(list);
-  // Not working; We can not assign a value in for in loop
-
-  // if and for inside list
-  const List<int> numbersList = [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5];
-  final List<int> squaredNumbersList = [
-    for (var num in numbersList)
-      if (!num.isNegative) num * num
-  ];
-  // print(squaredNumbersList); => [1, 4, 9, 16, 25]
+  // if and for same as in list 
 
   
 }
