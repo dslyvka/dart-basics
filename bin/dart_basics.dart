ec6ddc5b5dafd0fe7d4dea4599be38df8b8a25e6
dslyvka <daniilslyvka@gmail.com>
2023-03-18 01:03:48 +0200

    iterable

--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,33 +1,14 @@
 void main(List<String> arguments) {
-  Map<String, int> fruitsAndCalories = {"apple": 200, "strawberry": 50};
-  fruitsAndCalories["apple"] = 300; // => {"apple": 300, "strawberry": 50};
-  fruitsAndCalories["pineapple"] =
-      600; // => {apple: 300, strawberry: 50, pineapple: 600}
+  List<int> list = [1, 2, 3];
+  var reversed = list.reversed;
+  // Iterable<int> type and (3, 2, 1) iterable object
 
-  Map<String, int> fruitsAndCalories1 = {"apple": 200, "strawberry": 50};
-  Map<String, int> fruitsAndCalories2 = {"pear": 200, "peach": 50};
-  Map<String, int> fruitsAndCalories3 = {
-    ...fruitsAndCalories1,
-    ...fruitsAndCalories2
-  }; // => {apple: 200, strawberry: 50, pear: 200, peach: 50}
+  reversed = reversed.toList();
+  // [3, 2, 1] iterable object like list but still Iterable<int> type
 
-  Map<String, int> fruitsAndCalories4 = {
-    "apple": 200,
-    "strawberry": 50,
-    "apple": 250
-  }; // => {apple: 250, strawberry: 50}
+  List<int> reversedList = [...reversed];
 
-  // Looping over the map elements
-  for (var key in fruitsAndCalories4.keys) {
-    print(key); // => key
-    print(fruitsAndCalories4[key]); // => value
-  }
+  Iterable<String> myIterable = ['a', "b", "c"];
 
-  for (var value in fruitsAndCalories4.values) {
-    print(value);
-  }
-
-  for (var entry in fruitsAndCalories4.entries) {
-    print("${entry.key} ${entry.value}");
-  }
+  // To be continued...
 }
